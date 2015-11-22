36dc1643ac1e3c702b21fd64fd951cd330bd9dbf
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-22 15:47:00 +0000

    create_message method

--- message.rb
+++ message.rb
@@ -1,13 +1,29 @@
 class Message
-  def initialize(title, content)
-    title = @title
-    content = @content
+  attr_writer :create_message
+  
+  def initialize
     @messages = {}
   end
   
   def create_message
+    time = Time.now
+    puts "Creating a message."
+    puts "Enter a title."
+    title = gets.chomp
+    puts "Enter a message"
+    content = gets.chomp
+    message = [content, time]
+    @messages[title] = message 
+    puts ".:MESSAGE CREATED:."
   end
   
   def show_messages
+    puts "MESSAGES"
+    
   end
 end
+
+mail = Message.new
+mail.create_message
+
+
