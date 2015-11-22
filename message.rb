0da38fff70adbed28bfb38e9734425b4cc449a47
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-22 15:28:34 +0000

    Add initialize

--- message.rb
+++ message.rb
@@ -1,3 +1,13 @@
 class Message
+  def initialize(title, content)
+    title = @title
+    content = @content
+    @messages = {}
+  end
   
+  def create_message
+  end
+  
+  def show_messages
+  end
 end
